dd09d5e17e0762f71423c9d228e5614cc796d0e9
João Otávio de Farias de Oliveira <joaootaviodefariasdeoliveira@Joaos-MacBook-Air.local>
2023-10-09 15:19:44 -0300

    Init

---
+++ src/Trinca.AgendaChurrasco.Api/Controllers/ChurrascoController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Trinca.AgendaChurrasco.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ChurrascoController : ControllerBase
+{
+    
+}
